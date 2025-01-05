a51e4143001ee095ce41f54afac05ffe45ea2113
abdullox0900 <abdullox0900@gmail.com>
2025-01-05 22:14:42 +0500

    update error

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 async function fetchAboutData() {
     try {
-        const response = await fetch('http://176.124.210.146:3000/api/about')
+        const response = await fetch('https://a-abdullox.uz/api/about')
         const data = await response.json()
         const aboutData = data.docs[0]
 
@@ -12,19 +12,19 @@ async function fetchAboutData() {
         document.getElementById('form-title').textContent = aboutData?.form?.title
         document.getElementById('form-subtitle').textContent = aboutData?.form?.subtitle
 
-        document.getElementById('image1').src = 'http://176.124.210.146:3000/' + aboutData?.image1?.url
-        document.getElementById('image2').src = 'http://176.124.210.146:3000/' + aboutData?.image2?.url
+        document.getElementById('image1').src = 'https://a-abdullox.uz' + aboutData?.image1?.url
+        document.getElementById('image2').src = 'https://a-abdullox.uz' + aboutData?.image2?.url
 
         document.getElementById('title-card').textContent = aboutData?.cards_title
 
         const cardsContainer = document.getElementById('about-cards')
         aboutData.cards.forEach(card => {
-            console.log('http://176.124.210.146:3000/' + card.icon.url)
+            console.log('https://a-abdullox.uz' + card.icon.url)
             cardsContainer.innerHTML += `
                 <div class="relative">
                     <div class="bg-white rounded-2xl shadow-lg p-8 relative z-10 h-full">
                         <div class="w-12 h-12 bg-brand-100 rounded-2xl flex items-center justify-center mb-6">
-                                <img src="${'http://176.124.210.146:3000/' + card?.icon?.url}" alt="${card?.title}-icon">
+                                <img src="${'https://a-abdullox.uz' + card?.icon?.url}" alt="${card?.title}-icon">
                         </div>
                         <h3 class="text-xl font-bold text-gray-900 mb-4">${card?.title}</h3>
                         <p class="text-gray-600">
@@ -52,7 +52,7 @@ document.getElementById('ctaForm').addEventListener('submit', async function (e)
     }
 
     try {
-        const response = await fetch('http://176.124.210.146:3000/api/forms', {
+        const response = await fetch('https://a-abdullox.uz/api/forms', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
